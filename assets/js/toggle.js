12c7272aa2589515fcdb0d9bbe81894d7e724fd3
FrancoCF <franco.alfaro@comunidadfeliz.cl>
2020-03-13 13:55:18 -0300

    linking

---
+++ assets/js/toggle.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementsByClassName("knowMore");
+  if (x[0].style.display === "none") {
+    x[0].style.display = "block";
+  } else {
+    x[0].style.display = "none";
+  }
+}
+  
